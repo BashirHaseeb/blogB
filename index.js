4f683d282cfffd7a71a89d70f2a87150d8c9288b
Falcon-Abdullah <ch.ma.bashir@gmail.com>
2024-12-07 11:30:54 +0500

    done1

--- index.js
+++ index.js
@@ -9,7 +9,28 @@ const app = express();
 const PORT = 5000;
 
 // Middleware to parse JSON requests
-app.use(cors());
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            'https://lms-frontend-coral.vercel.app',
+            'http://localhost:3000'
+        ]; // List of allowed origins
+        if (!origin || allowedOrigins.includes(origin)) {
+            // Allow requests with a valid origin or no origin (e.g., server-side requests)
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true, // Allow cookies and credentials
+};
+
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 connectDB();
@@ -18,6 +39,8 @@ app.use('/user', userR)
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Done" });
 });
+
+app.options('*', cors(corsOptions));
 
 // Start server
 app.listen(PORT, () => {
